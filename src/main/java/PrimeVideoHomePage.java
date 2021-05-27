b0a771db7cc99848b0354135cf390d61bea1c934
formateur <f@ib.com>
2021-05-27 15:18:26 +0200

    added nintendo switch test

--- src/main/java/PrimeVideoHomePage.java
+++ src/main/java/PrimeVideoHomePage.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebElement;
 public class PrimeVideoHomePage {
     WebDriver driver;
 
+    By accueilButtonSelector = By.id("pv-nav-home");
+
     public PrimeVideoHomePage(WebDriver driver) {
         this.driver = driver;
     }
 
     public boolean isHomeButtonDisplayed() {
-        WebElement accueilButton = driver.findElement(By.id("pv-nav-home"));
-        return accueilButton.isDisplayed();
+        return driver.findElement(accueilButtonSelector).isDisplayed();
     }
 }
