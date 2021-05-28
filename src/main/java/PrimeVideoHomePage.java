2ce5bae0217d9303b1d7cefa42c8e5bd3cbfa464
christellesaade <81761686+christellesaade@users.noreply.github.com>
2021-05-28 10:03:31 +0200

    Revert "added nintendo switch test"

--- src/main/java/PrimeVideoHomePage.java
+++ src/main/java/PrimeVideoHomePage.java
@@ -5,13 +5,12 @@ import org.openqa.selenium.WebElement;
 public class PrimeVideoHomePage {
     WebDriver driver;
 
-    By accueilButtonSelector = By.id("pv-nav-home");
-
     public PrimeVideoHomePage(WebDriver driver) {
         this.driver = driver;
     }
 
     public boolean isHomeButtonDisplayed() {
-        return driver.findElement(accueilButtonSelector).isDisplayed();
+        WebElement accueilButton = driver.findElement(By.id("pv-nav-home"));
+        return accueilButton.isDisplayed();
     }
 }
