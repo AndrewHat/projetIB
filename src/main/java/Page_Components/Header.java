6d7b04eaba20e622c0a3adf9aa881c763498b91c
elmehdiBH <81761688+elmehdiBH@users.noreply.github.com>
2021-05-31 11:28:13 +0200

    Jenkins File Add

---
+++ src/main/java/Page_Components/Header.java
@@ -0,0 +1,12 @@
+package Page_Components;
+
+import org.openqa.selenium.By;
+
+public class Header {
+
+
+    By searchTextboxSelector = By.id("twotabsearchtextbox");
+    By searchButtonSelector = By.id("nav-search-submit-button");
+
+
+}
--- src/test/java/TestGIT.java
+++ src/test/java/TestGIT.java
@@ -1,9 +1,8 @@
+import PageObjects.HomePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -32,22 +31,36 @@ public class TestGIT {
         // Arrange
 
         // Act
-        By hamburgerButtonSelector = By.id("nav-hamburger-menu");
-        driver.findElement(hamburgerButtonSelector).click();
-
-        By amazonPrimeVideoSelector = By.cssSelector(".hmenu-item[data-menu-id='2']");
-
-        WebDriverWait wait = new WebDriverWait(driver, 2);
-        wait.until(ExpectedConditions.elementToBeClickable(amazonPrimeVideoSelector));
-        driver.findElement(amazonPrimeVideoSelector).click();
-
-        By filmsButtonSelector = By.cssSelector(".hmenu-visible[data-parent-menu-id='1'] li:nth-of-type(4)");
-        wait.until(ExpectedConditions.elementToBeClickable(filmsButtonSelector));
-        driver.findElement(filmsButtonSelector).click();
+        HomePage amazonHomePage = new HomePage(driver);
+        boolean isHomeButtonDisplayed = amazonHomePage
+                .openAllCategories()
+                .openAmazonPrimeVideo()
+                .openFilms()
+                .isHomeButtonDisplayed();
 
         // Asserts
+        Assert.assertTrue(isHomeButtonDisplayed, "Le button accueil n'est pas visible");
+    }
 
-        WebElement accueilButton = driver.findElement(By.id("pv-nav-home"));
-        Assert.assertTrue(accueilButton.isDisplayed(), "Le button accueil n'est pas visible");
+    @Test
+    public void testNintendoSwitch() {
+        String keyword = "Nintendo Switch";
+
+        HomePage homePage = new HomePage(driver);
+        String productTitle = homePage.search(keyword).openProduct(0).getProductTitle();
+
+        Assert.assertTrue(productTitle.contains(keyword), "The product title [" + productTitle + "] does not contain the keyword [" + keyword + "]");
+    }
+
+    @Test
+    public void testLuis() {
+        By barreRecherche = By.id("twotabsearchtextbox");
+        driver.findElement(barreRecherche).sendKeys("Playstation 5" + Keys.ENTER);
+    }
+  
+    @Test
+    public void testXbox() {
+        By meilleuresVentes = By.cssSelector("#nav-xshop > a");
+        driver.findElement(meilleuresVentes).click();
     }
 }
